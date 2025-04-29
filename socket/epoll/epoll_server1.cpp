8fcc87ed9986d6613f62f4845191b05704f47838
centos9 <2440580838@qq.com>
2025-04-29 23:10:00 +0800

    23.00

--- socket/epoll/epoll_server1.cpp
+++ socket/epoll/epoll_server1.cpp
@@ -24,25 +24,57 @@ int main(int argc,char *argv[]){
     ERROR_CHECK(epfd,-1,"epoll_create server");
 
     struct epoll_event ev;
-    ev.data.fd=STDIN_FILENO;
-    ev.events=EPOLLIN;
-    epoll_ctl(epfd,EPOLL_CTL_ADD,ev.data.fd,&ev);
     ev.data.fd=sockfd;
     ev.events=EPOLLIN;
     epoll_ctl(epfd,EPOLL_CTL_ADD,ev.data.fd,&ev);
-
+    int netfd=-1;
+    char buf[4096]={0};
     while(1){
         struct epoll_event readyEvents[MAXEVENTS];
         int readyNum=epoll_wait(epfd,readyEvents,MAXEVENTS,-1);
         for(int i=0;i<readyNum;++i){
-            if(readyEvents[i].data.fd=sockfd){
+            if(readyEvents[i].data.fd==sockfd){
                 epoll_ctl(epfd,EPOLL_CTL_DEL,sockfd,nullptr);
-                sockaddr_in clientAddr;
-                socklen_t socklen;
-                int netfd=accept(sockfd,(struct sockaddr*)&clientAddr,&socklen);
+                netfd=accept(sockfd,nullptr,nullptr);
                 ev.data.fd=netfd;
                 ev.events=EPOLLIN;
                 epoll_ctl(epfd,EPOLL_CTL_ADD,ev.data.fd,&ev);
+                ev.data.fd=STDIN_FILENO;
+                ev.events=EPOLLIN;
+                epoll_ctl(epfd,EPOLL_CTL_ADD,ev.data.fd,&ev);
+                printf("connected!\n");
+            }
+            else if(netfd!=-1&&readyEvents[i].data.fd==netfd){
+                memset(buf,0,sizeof(buf));
+                ssize_t sret=recv(netfd,buf,sizeof(buf),0);
+                if(sret==0){
+                    printf("client close connected\n");
+                    ev.events=EPOLLIN;
+                    ev.data.fd=sockfd;
+                    epoll_ctl(epfd,EPOLL_CTL_DEL,STDIN_FILENO,nullptr);
+                    epoll_ctl(epfd,EPOLL_CTL_DEL,netfd,nullptr);
+                    epoll_ctl(epfd,EPOLL_CTL_ADD,sockfd,&ev);
+                    close(netfd);
+                    netfd=-1;
+                    break;
+                }
+                printf("sret = %ld,buf = %s\n",sret,buf);
+            }
+            else if(netfd!=-1&&readyEvents[i].data.fd==STDIN_FILENO){
+                memset(buf,0,sizeof(buf));
+                ssize_t sret=read(STDIN_FILENO,buf,sizeof(buf));
+                if(sret==0){
+                    printf("kick client\n");
+                    ev.events=EPOLLIN;
+                    ev.data.fd=sockfd;
+                    epoll_ctl(epfd,EPOLL_CTL_DEL,STDIN_FILENO,nullptr);
+                    epoll_ctl(epfd,EPOLL_CTL_DEL,netfd,nullptr);
+                    epoll_ctl(epfd,EPOLL_CTL_ADD,sockfd,&ev);
+                    close(netfd);
+                    netfd=-1;
+                    break;
+                }
+                send(netfd,buf,strlen(buf),0);
             }
         }
     }
